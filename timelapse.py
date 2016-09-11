df19992f94284746e2de09789ce6de97c44832cc
R. Mutt <rrmutt@gmail.com>
2016-09-11 16:04:55 -0700

    checkpoint

--- timelapse.py
+++ timelapse.py
@@ -9,29 +9,24 @@ import subprocess
 setup = []
 setup.append("gphoto2 --set-config capture=on".split())
 setup.append("gphoto2 --set-config focuslock=on".split())
+setup.append("gphoto2 --set-config assistlight=0".split())
+# 1024 x 768?
+setup.append("gphoto2 --set-config imagesize=3".split())
+# save to SRAM
 setup.append("gphoto2 --set-config capturetarget=0".split())
 setup.append("gphoto2 --set-config zoom=8".split())
-setup.append('gphoto2 --capture-image-and-download -I 2 -F 900 --filename jpg/%Y%m%d%H%M%S.jpg'.split())
+
+duration_hrs = 0.25
+hrs_b4_sunset = 1.5
+hrs_before_sunset = 8.01
+frame_delay_s = 2 
+# destination directory: raw files in subdir here named by date
+dest_dir = "/home/pi/cam/jpg/" 
 
 
-#http://www.moreno.marzolla.name/software/linux-time-lapse/
+nframes = int(duration_hrs*3600/frame_delay_s)
 
-try:
-     result = subprocess.check_output(['gphoto2', '--auto-detect'])
-except subprocess.CalledProcessError:
-     print "Camera not detected!"
-else:
-     print result
-     
-for cmd in setup:
-     print cmd
-     result = subprocess.check_output(cmd)
-     print result
-     time.sleep(0.5)
-     
-exit()
-
-# first wait for 1 hour before sunset.
+# first wait for hrs_before_sunset.
 #Calculate sunset
 import ephem
 from datetime import datetime, timedelta
@@ -50,12 +45,12 @@ print ephem.localtime(o.next_rising(s)).ctime()
 sunset_ctime = ephem.localtime(o.next_setting(s)).ctime()
 sunset_dtime = datetime.strptime(sunset_ctime, "%a %b %d %H:%M:%S %Y")
 # one hour before sunset
-then = sunset_dtime - timedelta(hours = 1)
+then = sunset_dtime - timedelta(hours = hrs_before_sunset)
 
-flag = False
+flag = True
 while flag:
     now = datetime.now()
-    print "time before 1 hour before sunset: " +  str(then - now)
+    print "waiting for " + str(then) + " (remaining)" +  str(then - now)
     sys.stdout.flush()
     
     if  now < then:
@@ -63,61 +58,44 @@ while flag:
     else:
         flag = False
         
-print "Almost time for sunset!"
-#exit()
-
-#Reset hub
-# sudo ./usbreset /dev/bus/usb/008/004
-
-#time.sleep(3*3600)
-
-zoomcall = ['gphoto2','--set-config', 'Zoom=4']
-print ' '.join(zoomcall)
-subprocess.call(zoomcall)
+print "Done waiting, let's go!"
 
 
-camcall1 = ['gphoto2', '--capture-image-and-download']
-camcall1.append('--filename')
-camcall1.append('/home/jtf/cam/jpg/raw/%m%d%H%M%S.jpg')
+#http://www.moreno.marzolla.name/software/linux-time-lapse/
 
+try:
+     result = subprocess.check_output(['gphoto2', '--auto-detect'])
+except subprocess.CalledProcessError:
+     print "Camera not detected!"
+else:
+     print result
+     
+for cmd in setup:
+     print cmd
+     result = subprocess.check_output(cmd)
+     print result
+     time.sleep(0.5)
 
-camcall = ['gphoto2', '--capture-image-and-download']
-camcall.append('--filename')
+# make deistination directory
+#datestr = time.strftime('%2y-%3j') #year followed by day in year
+daystr =  time.strftime('%2y-%2m-%2d') #year followed by  mo, day
+dirpath = os.path.join(dest_dir,daystr)
 
-# fsize = os.path.getsize()
-
-#camcall = 'ls'
-#camargs  = 'camraw'
-
-datestr = time.strftime('%2y-%3j') #year followed by day in year
-daystr =  time.strftime('%2y-%2m-%d') #year followed by  mo, day
-
-dirpath = "/home/pi/cam/jpg/" + datestr
-
-
-print "starting cam script"
 
 if not os.path.exists(dirpath):
     print "creating jpg dir " + dirpath
     os.mkdir(dirpath)
 
-keepgoing = True
-smallcount = 0
-while(keepgoing):
-    # first make filename from the current time
-    fpath = dirpath + '/' + time.strftime('%2H%2M%2S') + ".jpg"
-    tempcall = camcall + [fpath]
-    subprocess.call(tempcall)
-    print ' '.join(tempcall)
-    time.sleep(1)
 
-    # now stat the current file. If too small, then quit
-    if os.path.exists(fpath):
-        fsize = os.path.getsize(fpath)
-        print "created %s, %d kb" % ( fpath, fsize)
-        if fsize < 900000: # it's dark out
-            keepgoing = False
-            
-#movcall = ['/home/jtf/cam/movscript.py', dirpath]
-#print ' '.join(movcall)
-#subprocess.call(movcall)
+camcall = ['gphoto2', '--capture-image-and-download']
+camcall.append('-I')
+camcall.append('2')
+camcall.append('-F')
+camcall.append(str(nframes))
+camcall.append('--filename')
+camcall.append(os.path.join(dirpath,'%m%d%H%M%S.jpg'))
+print camcall
+
+result = subprocess.check_output(camcall)
+print result
+
